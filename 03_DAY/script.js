b46c1bbf5f9e7bf21e8d8ea1205bf5ed4525e6d7
Sumit Sharma <100469632+sumitkumarsharm@users.noreply.github.com>
2025-04-03 22:40:52 +0530

    Today i Learn installing and unstalling a packages nodemodules devDependencies

---
+++ 03_DAY/script.js
@@ -0,0 +1,11 @@
+// installing packages
+//  npm install # Install a package
+
+// unistalling packages
+// npm uninstall # Remove a package
+
+// Remove an item from your favorite packages
+// npm unstar # Remove an item from your favorite packages
+
+// how to Install a specific version of a package
+// npm install package_name@version # Install a specific version of a package
